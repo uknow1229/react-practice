d0d77b5ea0a3468e5bae349e0dc2c0e744619c99
saya <uknow.love1229@gmail.com>
2023-08-16 13:15:24 +0900

    add: Example

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Example = () => {
+  let ValArry = useState(0);
 
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          const setFn = ValArry[1];
+          setFn(e.target.value);
+        }}
+      />
+      = {ValArry[0]}
     </>
   );
 };
